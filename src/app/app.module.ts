0badf3374bbb07ad3408aaf6ba4bc39e2115761d
jocelyn Milon <jocelyn.milon@gmail.com>
2018-07-24 01:00:18 +0200

    creation projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent,
+    CarouselComponent,
+    AccueilComponent,
+    FormulaireComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulaire/formulaire.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulaire',
+  templateUrl: './formulaire.component.html',
+  styleUrls: ['./formulaire.component.css']
+})
+export class FormulaireComponent implements OnInit {
+
+  reservationForm: FormGroup;
+  arrive = new Date();
+  propriete = ['Los Angeles', 'New york', 'Guadeloupe', 'Martinique', 'France'];
+  peopleOptions = [1, 2, 3, 4, 5, 6, 7, 8];
+  depart = new Date();
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.reservationForm = this.fb.group({
+      arrive: new Date(),
+      depart: new Date(),
+      propriete: '',
+      peopleOptions: 1,
+     
+    })
+  }
+}
